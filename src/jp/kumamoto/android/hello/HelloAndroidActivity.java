66b5f161d15261860ff6a034a7b45ac7dd6a05d1
Owner <Owner@Owner-PC>
2011-12-23 13:27:45 +0900

    a

--- src/jp/kumamoto/android/hello/HelloAndroidActivity.java
+++ src/jp/kumamoto/android/hello/HelloAndroidActivity.java
@@ -9,5 +9,6 @@ public class HelloAndroidActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        //aaa
     }
 }
